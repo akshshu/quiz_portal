78e5f28d5accde42f2070acd6efe67d7e2c69bd0
akshshu <akshshu@gmail.com>
2020-04-01 02:09:03 +0530

    answer added and logic for evaluation

--- ques.js
+++ ques.js
@@ -17,16 +17,17 @@ var option = [["in-service expansion", "unlimited number of stations", "both (a)
 ["1963,USA", "1965,Germany", "1950,USA", "1950 Japan"],
 ["in-service expansion", "unlimited number of stations", "both (a) and (b)", "unlimited distance"],
 ["Unshielded Twisted pair", "Ubiquitious Teflon port", "Uniformly Terminating port", "Unshielded T-connector port"],
-["the nodes", "the server", "the hubs", "a separate pc that manages the network"]]
+["the nodes", "the server", "the hubs", "a separate pc that manages the network"]];
+var ans = ["in-service expansion", "Unshielded Twisted pair", "the hubs", "Entropy", "All of Those", "Entropy", "1950,USA", "in-service expansion", "Unshielded Twisted pair", "the hubs"];
 var tag = "<div class=\"col-md-12\"><h3>";
-var opt_tag = "<label><input type=\"radio\" name=\"";
+var opt_tag = '<input type="radio" name=';
 var y = document.getElementById("main");
 var i, j;
+var count = 0;
 var row_h = y.innerHTML;
 for (i = 0; i < 10; i++) {
     row_h += "<div class=\"row\"></div>";
 }
-console.log(row_h);
 y.innerHTML = row_h;
 var x = document.getElementsByClassName("row");
 function sc(val) {
@@ -34,11 +35,29 @@ function sc(val) {
     var l = "\""
     var j;
     for (j = 0; j < 4; j++) {
-        s += opt_tag + val + l + " > " + option[val][j] + "</label ></br > ";
+        s += opt_tag + '"question' + i + '" value="' + option[val][j] + '"><label>' + option[val][j] + '</label></br>';
     }
     return s;
 }
 for (i = 0; i < x.length; i++) {
+    if (i == x.length - 1) {
+        x[i].innerHTML = tag + que[i] + "</h3>" + sc(i) + '<div class="btn" id="button">Submit</div>';
 
-    x[i].innerHTML = tag + que[i] + "</h3>" + sc(i);
+    }
+    else {
+
+        x[i].innerHTML = tag + que[i] + "</h3>" + sc(i);
+    }
+
+}
+document.getElementById('button').addEventListener('click', evaluate);
+function evaluate() {
+    for (i = 0; i < x.length; i++) {
+        user_opt = (document.querySelector('input[name=question' + i + ']:checked') || {});
+        if (user_opt.value == ans[i]) {
+            count += 1;
+        }
+    }
+    console.log(count);
+    alert(count + " correct");
 }
